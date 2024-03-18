28d000a085770d6e6655b9a0775358deebf41bbc
Eresedia Chademuwiri <chademuwirie@icloud.com>
2024-03-18 11:15:17 +0200

    modified:   script.js

--- script.js
+++ script.js
@@ -21,6 +21,17 @@ function addNewNote(text = ''){
         const main = note.querySelector('.main')
         const textArea = note.querySelector('.textarea')
 
+        deleteBtn.addEventListener('click', () =>{
+            note.remove()
+        })
+
+        editBtn.addEventListener('click', () =>{
+            main.classList.toggle('hidden')
+            textArea.classList.toggle('hidden')
+        })
+        
+
+
         document.body.appendChild(note)
 
 }
